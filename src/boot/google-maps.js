f5536f220e9bd237022cfa1a5679ae8b91866f43
JuniorMCF <juniorcastillon6@gmail.com>
2022-07-14 15:09:00 -0500

    api key google maps prove of concept

--- src/boot/google-maps.js
+++ src/boot/google-maps.js
@@ -4,7 +4,8 @@ import VueGoogleMaps from '@fawmi/vue-google-maps';
 export default boot(({ app }) => {
   app.use(VueGoogleMaps, { // 🤿 Vue App. Please install Vue Google Maps
     load: {
-      key: 'AIzaSyB109lpUN1h-3pn42zqp4TcLIqnM_0W0QY', // 🤿 I don't have a google key, so leave it blank for now
+      //key: 'AIzaSyB109lpUN1h-3pn42zqp4TcLIqnM_0W0QY', // 🤿 I don't have a google key, so leave it blank for now
+      key: 'AIzaSyB6sTrxyXA_2fKGdWr8mvJKOFy0ysbxv-8',
       libraries: "places"
     },
   });
